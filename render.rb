23b2531d3a32132503cf1bab3650729cd5cba21c
Christopher Schuster <cs@livoris.net>
2016-10-12 18:46:57 -0700

    added option to run neato on Windows

--- render.rb
+++ render.rb
@@ -3,10 +3,6 @@
 require 'csv'
 require 'gv'
 
-g = GV::Graph.open('G', :undirected)
-g[:rankdir] = 'LR'
-g[:outputorder] = 'edgesfirst'
-
 class Party
   attr_reader :name
   attr_reader :answers
@@ -14,10 +10,9 @@ class Party
 
   @@parties = []
 
-  def initialize(g, name)
+  def initialize(name)
     @name = name
     @answers = []
-    @node = g.node(name, shape: 'ellipse', margin: '.04', fontsize: '11', style: 'filled', fillcolor: '#ffffff', fontname: 'opensans')
     @@parties << self
   end
 
@@ -33,13 +28,19 @@ class Party
     return value
   end
 
+  def self.create_nodes(g)
+    @@parties.each do |p|
+      g.puts '  "' + p.name + '" [fillcolor="#ffffff",fontname=opensans,fontsize=11,height=0.5,margin=.04,shape=ellipse,style=filled];'
+    end
+  end
+
   def self.create_edges(g)
     for i in 0...@@parties.size
       for j in (i+1)...@@parties.size
         p1 = @@parties[i]
         p2 = @@parties[j]
         distance = p1.similar(p2).to_f / 8
-        g.edge('', p1.node, p2.node, arrowtype: 'none', color: '#aaaaaa', len: distance.to_s)
+        g.puts '  "' + p1.name + '" -- "' + p2.name + '" [arrowtype=none,color="#aaaaaa",len=' + distance.to_s + '];'
       end
     end
   end
@@ -55,12 +56,26 @@ first = true
 CSV.foreach(ARGV[0]) do |row|
   if first then
     first = false
-    row.each { |party_name| Party.new(g, party_name) }
+    row.each { |party_name| Party.new(party_name) }
   else
     Party.answer row
   end
 end
 
-Party.create_edges(g)
+dotname = File.basename(ARGV[0], ".csv") + ".dot"
+pngname = File.basename(ARGV[0], ".csv") + ".png"
 
-g.save('wahlomat-vis.png', 'png', 'neato')
+g = open(dotname, "w")
+g.puts 'graph {'
+g.puts '  graph [outputorder=edgesfirst,rankdir=LR];'
+g.puts '  node [label="\\N"];'
+Party.create_nodes(g)
+Party.create_edges(g)
+g.puts '}'
+g.close
+
+if (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/) then
+  `"C:\\Program Files\\Graphviz2.38\\bin\\neato.exe" #{dotname} -Tpng -o #{pngname}`
+else
+  `neato #{dotname} -Tpng -o #{pngname}`
+end
